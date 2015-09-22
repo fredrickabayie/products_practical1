e4041ef2e56c5fd6527236af8c6509d869e9d2e3
Fredrick Abayie <fredrick.abayie@gmail.com>
2015-09-22 00:35:37 +0000

    Evening
    Making changes

--- views/index.php
+++ views/index.php
@@ -10,53 +10,28 @@ and open the template in the editor.
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-sale=1">
         <link rel="stylesheet" href="../packages/jquery.mobile-1.4.5.min.css">
-        <script src="../packages/jquery-2.1.4.min.js"></script>
-        <script src="../packages/jquery.mobile-1.4.5.min.js"></script>
-        <link rel="stylesheet" href="../packages/custom.css"/>
-        <link rel="stylesheet" href="../packages/custom.min.css"/>
+        <script type="text/javascript" src="../packages/jquery-2.1.4.min.js"></script>
+        <script type="text/javascript" src="../packages/jquery.mobile-1.4.5.min.js"></script>
+<!--        <link rel="stylesheet" href="../packages/custom.css"/>
+        <link rel="stylesheet" href="../packages/custom.min.css"/>-->
         <script type="text/javascript" src="../controllers/handlers.js"></script>
     </head>
     
     <body>
         
-        <div data-role="page" data-theme="a" id="pageone">
+        <div data-role="page" data-theme="b" id="pageone">
             <div data-role="header" data-position="fixed">
                 <h1>Product Guru</h1>
-                <a href="#dialog_page" style="width: 70px" data-icon="plus" data-iconpos="notext" data-rel="dialog" data-transition="pop"></a>
-                <input type="button" value="About"/>
                 <div data-role="navbar">
                     <ul> 
-                        <!--<li><a href="#anylink" class="ui-btn-active ui-icon-home ui-state-persist">Home</a></li>--> 
-                        <li><a href="#anylink" class="ui-btn-active ui-state-persist">Page Two</a></li>
-                        <li><a href="#anylink" class="ui-btn-active ui-state-persist">Search</a></li>
+                        <li><a href="#dialog_page" data-role="button" data-transition="turn" data-icon="plus" class="ui-btn-active ui-state-persist">Add Product</a></li>
+                        <li><a href="#pagetwo" data-transition="flip" data-icon="bullets" class="ui-btn">View Products</a></li>
+                        <li><a href="#pagethree" data-transition="flow" class="ui-btn">About</a></li>
                     </ul>
                 </div>
             </div>
             
-            <div role="main" class="ui-content">
-                                
-                <!--The list displaying the list of products in the database-->
-                <ul data-role="listview" id="prod_display" data-inset="true">
-                    
-                </ul>
-                
-            </div>
-            
-        <div data-role="footer" data-position="fixed" style="">
-            <h1>Designed by Okokroko</h1>
-        </div>
-            
-        </div>  
-        
-        <!--The modal for adding a new product-->
-                <div data-role="dialog" id="dialog_page">
-                    
-                    <div data-role="header">
-                        <h1>New Product</h1>
-                    </div>
-                    
-                    <div data-role="content">
-                        
+             <div data-role="content"> 
                         <div data-role="fieldcontain" class="ui-field-contain" >
                           <label for="prod_name">Name:</label>
                           <input type="text" name="prod_name" id="prod_name">
@@ -67,16 +42,74 @@ and open the template in the editor.
                           <label for="prod_barcode">Barcode:</label>
                           <input type="text" name="prod_barcode" id="prod_barcode">      
                         </div>
-                        
-                    </div>
-                    
-                    <div data-role="footer">
+                 
+                 <div data-role="footer">
                         <input type="button" id="submit_btn" value="Save">
                     </div>
+                        
+              </div>
+               
+        <div data-role="footer" data-position="fixed" style="">
+            <h1>Designed by Okokroko</h1>
+        </div>
+            
+        </div>
+        
+        <!--The modal for adding a new product-->
+                <div data-role="page" id="pagetwo">
+                    
+                    <div data-role="header" data-position="fixed">
+                <h1>Product Guru</h1>
+                <!--<a href="#dialog_page" style="width: 70px" data-icon="plus" data-iconpos="notext" data-rel="dialog" data-transition="pop"></a>-->
+                <div data-role="navbar">
+                    <ul> 
+                        <li><a href="#pageone" data-role="button" data-transition="turn" data-icon="plus" class="ui-btn">Add Product</a></li>
+                        <li><a href="#pagetwo" data-transition="flip" data-icon="bullets" class="ui-btn-active ui-state-persist">View Products</a></li>
+                        <li><a href="#pagethree" data-transition="flow" class="ui-btn">About</a></li>
+                    </ul>
+                </div>
+            </div>
+                    
+                     <div role="main" class="ui-content">
+                    <!--The list displaying the list of products in the database-->
+                    <ul data-role="listview" id="prod_display" data-inset="true">
+
+                    </ul>
+                 </div>
+                   
+                    <div data-role="footer" data-position="fixed">
+            <h1>Designed by Okokroko</h1>
+        </div>
+                    
                     
                 </div>
         
-        
+                        <div data-role="page" id="pagethree">
+                    
+                    <div data-role="header" data-position="fixed">
+                <h1>Product Guru</h1>
+                <!--<a href="#dialog_page" style="width: 70px" data-icon="plus" data-iconpos="notext" data-rel="dialog" data-transition="pop"></a>-->
+                <div data-role="navbar">
+                    <ul> 
+                        <li><a href="#pageone" data-role="button" data-transition="turn" data-icon="plus" class="ui-btn">Add Product</a></li>
+                        <li><a href="#pagetwo" data-transition="flip" data-icon="bullets" class="ui-btn">View Products</a></li>
+                        <li><a href="#pagethree" data-transition="flow" class="ui-btn-active ui-state-persist">About</a></li>
+                    </ul>
+                </div>
+            </div>
+                    
+                     <div role="main" data-role="content">
+                         <div data-role="collapsible">
+                             
+                         </div>
+                    </div>
+                   
+                    <div data-role="footer" data-position="fixed" style="">
+            <h1>Designed by Okokroko</h1>
+        </div>
+                    
+                    
+                </div>
         
         
     </body>
