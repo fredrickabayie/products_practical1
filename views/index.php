d326c198ad98e3f9e6f153bc63f0de5489fff351
Fredrick Abayie <fredrick.abayie@gmail.com>
2015-09-22 14:25:46 +0000

    Editing

--- views/index.php
+++ views/index.php
@@ -13,20 +13,21 @@ and open the template in the editor.
         <script type="text/javascript" src="../packages/jquery-2.1.4.min.js"></script>
         <script type="text/javascript" src="../packages/jquery.mobile-1.4.5.min.js"></script>
         <link rel="stylesheet" href="../packages/custom.css"/>
-        <link rel="stylesheet" href="../packages/custom.min.css"/>
+        <!--<link rel="stylesheet" href="../packages/custom.min.css"/>--> 
         <script type="text/javascript" src="../controllers/handlers.js"></script>
     </head>
     
     <body>
         
-        <div data-role="page" data-theme="c" id="pageone">
+        <div data-role="page" data-theme="b" id="pageone">
             <div data-role="header" data-position="fixed">
+                <a href="#pageone" data-role="button">Home</a> 
                 <h1>Product Guru</h1>
                 <div data-role="navbar">
                     <ul> 
                         <li><a href="#dialog_page" data-role="button" data-transition="turn" data-icon="plus" class="ui-btn-active ui-state-persist">Add Product</a></li>
                         <li><a href="#pagetwo" data-transition="flip" data-icon="bullets" class="ui-btn">View Products</a></li>
-                        <li><a href="#pagethree" data-transition="flow" class="ui-btn">About</a></li>
+                        <li><a href="#pagethree" data-transition="flow" data-icon="gear" class="ui-btn">About</a></li>
                     </ul>
                 </div>
             </div>
@@ -56,7 +57,7 @@ and open the template in the editor.
         </div>
         
         <!--The modal for adding a new product-->
-                <div data-role="page" data-theme="b" id="pagetwo">
+                <div data-role="page" data-theme="c" id="pagetwo">
                     
                     <div data-role="header" data-position="fixed">
                 <h1>Product Guru</h1>
@@ -65,7 +66,7 @@ and open the template in the editor.
                     <ul> 
                         <li><a href="#pageone" data-role="button" data-transition="turn" data-icon="plus" class="ui-btn">Add Product</a></li>
                         <li><a href="#pagetwo" data-transition="flip" data-icon="bullets" class="ui-btn-active ui-state-persist">View Products</a></li>
-                        <li><a href="#pagethree" data-transition="flow" class="ui-btn">About</a></li>
+                        <li><a href="#pagethree" data-transition="flow" data-icon="gear" class="ui-btn">About</a></li>
                     </ul>
                 </div>
             </div>
@@ -84,23 +85,23 @@ and open the template in the editor.
                     
                 </div>
         
-                        <div data-role="page" id="pagethree">
+            <div data-role="page" id="pagethree" style="background-color: green">
                     
-                    <div data-role="header" data-position="fixed">
-                <h1>Product Guru</h1>
+                <div data-role="header" data-position="fixed" style="background-color: violet">
+                    <h1 style="font-size: 23px">Product Guru</h1>
                 <!--<a href="#dialog_page" style="width: 70px" data-icon="plus" data-iconpos="notext" data-rel="dialog" data-transition="pop"></a>-->
-                <div data-role="navbar">
+                <div data-role="navbar" style="background-color: indigo">
                     <ul> 
-                        <li><a href="#pageone" data-role="button" data-transition="turn" data-icon="plus" class="ui-btn">Add Product</a></li>
-                        <li><a href="#pagetwo" data-transition="flip" data-icon="bullets" class="ui-btn">View Products</a></li>
-                        <li><a href="#pagethree" data-transition="flow" class="ui-btn-active ui-state-persist">About</a></li>
+                        <li><a style="background-color: red; font-size: 19px" href="#pageone" data-role="button" data-transition="turn" data-icon="plus" class="ui-btn">Add Product</a></li>
+                        <li><a style="background-color: red; font-size: 19px" href="#pagetwo" data-transition="flip" data-icon="bullets" class="ui-btn">View Products</a></li>
+                        <li><a style="background-color: purple; font-size: 19px" href="#pagethree" data-transition="flow" data-icon="gear" class="ui-btn-active ui-state-persist">About</a></li>
                     </ul>
                 </div>
             </div>
                     
                      <div data-role="content">
                          <div data-role="collapsible">
-                             <h1>About Software</h1>
+                             <h1 style="font-style: italic">About Software</h1>
                              <p>This is a software to help SAFE personnel enter products into the system.</p>
                          </div>
                     </div>
